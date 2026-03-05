c9a20dd1279093a33ebcd63b91782b247dea1221
moxkxs <pby5ws@virginia.edu>
2026-03-05 14:05:04 -0500

    added search to run to create functioning program, cleaned up print statement in main

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for {} in file {}...", config.query, config.file_path);
+    println!("Searching for {} in {}...", config.query, config.file_path);
 
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
