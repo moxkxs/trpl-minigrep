b370c402e397ef3146fb311818fdcaabc34324c6
moxkxs <pby5ws@virginia.edu>
2026-03-05 13:02:05 -0500

    Added file reading (no error handling yet)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,4 +8,9 @@ fn main() {
     let file_path = &args[2];
 
     println!("Searching for {query} in file {file_path}...");
+
+    let contents = fs::read_to_string(file_path)
+        .expect("This should be able to read the file...");
+
+    println!("With text:\n{contents}");
 }
