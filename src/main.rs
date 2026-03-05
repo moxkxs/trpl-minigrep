2ee9eb3283f96622586200ccc93f359c31375072
moxkxs <pby5ws@virginia.edu>
2026-03-05 13:48:10 -0500

    externalized worker functions to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,31 +13,8 @@ fn main() {
 
     println!("Searching for {} in file {}...", config.query, config.file_path);
 
-    let contents = fs::read_to_string(config.file_path)
-        .expect("This should be able to read the file...");
-
-    println!("With text:\n{contents}");
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(
-            Config {
-                query,
-                file_path
-            }
-        )
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
 }
