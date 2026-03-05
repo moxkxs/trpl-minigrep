cd6758ef739f1dc844818700bae3cd6d17239633
moxkxs <pby5ws@virginia.edu>
2026-03-05 13:23:46 -0500

    restructured original code to utilize Config struct

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,29 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::new(&args);
 
-    println!("Searching for {query} in file {file_path}...");
+    println!("Searching for {} in file {}...", config.query, config.file_path);
 
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("This should be able to read the file...");
 
     println!("With text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config {
+            query,
+            file_path
+        }
+    }
 }
